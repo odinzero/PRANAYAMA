7f619fae10ed3b9d22ae04866adef52d6364e617
odinzero <alekseykravchenko1979@gmail.com>
2019-12-09 12:50:50 +0200

    update_09_1

---
+++ main/java/com/pranayama/basic/IName.java
@@ -0,0 +1,10 @@
+
+package com.pranayama.basic;
+
+public interface IName {
+    
+    String getName();
+    
+    void setName(final String name);
+    
+}
---
+++ main/java/com/pranayama/basic/IRunnable.java
@@ -0,0 +1,12 @@
+
+package com.pranayama.basic;
+
+public interface IRunnable<T> {
+    
+    public void startThread();
+    
+    public void stopThread();
+    
+    public void pauseThread();
+    
+}
